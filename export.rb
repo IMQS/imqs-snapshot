14bf66a59a1a84c35b8e96ae123ed186ff76fce2
Fritz Engelbrecht <fritz.engelbrecht@imqs.co.za>
2017-02-23 10:26:19 +0200

    Handeling deleting of old export atomicly.

--- export.rb
+++ export.rb
@@ -6,6 +6,7 @@ def export(args)
 	not_found = false
 	server_name = args[0]
 	snap_location = ''
+	server_location = ''
 	postgres_location = 'c:\\imqsvar\\postgres'
 	mongo_location = 'c:\\imqsvar\\mongo'
 	bin_location = 'c:\\imqsbin\\bin'
@@ -33,21 +34,36 @@ def export(args)
 	end
 
 	if server_name == 'backup'
-		snap_location = 'c:\\temp\\backup\\'
+		server_location = 'c:\\temp\\backup\\'
 	else
-		snap_location = "t:\\IMQS8_Data\\Servers\\#{server_name}\\"
+		server_location = "t:\\IMQS8_Data\\Servers\\#{server_name}\\"
 	end
 
-	if File.directory?(snap_location + 'Snapshot')
-		FileUtils.rm_rf(snap_location + 'Snapshot')
-	end
-	FileUtils.mkdir_p(snap_location + 'Snapshot')
+	snap_location = server_location + 'Snapshot\\'
 
-	if !File.directory?(snap_location + 'Imports')
-		FileUtils.mkdir_p(snap_location + 'Imports')
+	cleaner = Thread.new do
+		if File.directory?(snap_location)
+			loop do
+				moved = true
+				FileUtils.mv(snap_location, server_location + 'Snapshot_old') rescue moved = false
+				
+				if moved
+					break
+				end
+				sleep(1)
+			end
+
+			FileUtils.rm_rf(server_location + 'Snapshot_old')
+		end
 	end
 
-	snap_location += 'Snapshot\\'
+	if !File.directory?(server_location + 'Imports')
+		FileUtils.mkdir_p(server_location + 'Imports')
+	end
+
+	snap_location_new = server_location + 'Snapshot_new\\'
+	FileUtils.mkdir_p(snap_location_new)
+
 	postgres_location += '\\*'
 	mongo_location += '\\*'
 	bin_location += '\\*'
@@ -58,20 +74,23 @@ def export(args)
 	end
 
 	puts("Ziping up #{server_name} Postgres database")
-	cmd = "#{p7z} a #{snap_location}postgres_dump.7z -m0=lzma2 -mx0 #{postgres_location}"
+	cmd = "#{p7z} a #{snap_location_new}postgres_dump.7z -m0=lzma2 -mx0 #{postgres_location}"
 	`#{cmd}`
 
 	puts("Ziping up #{server_name} Mongo database")
-	cmd = "#{p7z} a #{snap_location}mongo_dump.7z -m0=lzma2 -mx0 #{mongo_location}"
+	cmd = "#{p7z} a #{snap_location_new}mongo_dump.7z -m0=lzma2 -mx0 #{mongo_location}"
 	`#{cmd}`
 
 	puts("Ziping up #{server_name} Binarys")
-	cmd = "#{p7z} a #{snap_location}bindumps.7z -m0=lzma2 -mx0 #{bin_location}"
+	cmd = "#{p7z} a #{snap_location_new}bindumps.7z -m0=lzma2 -mx0 #{bin_location}"
 	`#{cmd}`
 	
 	puts("Ziping up #{server_name} Configs")
-	cmd = "#{p7z} a #{snap_location}confdumps.7z -m0=lzma2 -mx0 #{conf_location}"
+	cmd = "#{p7z} a #{snap_location_new}confdumps.7z -m0=lzma2 -mx0 #{conf_location}"
 	`#{cmd}`
+
+	cleaner.join
+	FileUtils.mv(snap_location_new, snap_location)
 
 	puts('Exported system to ' + snap_location)
 
